85c1495a040dc5aac7e7d91b586483f21a3732b4
Martin Haupt <martin23haupt@gmail.com>
2019-06-04 21:23:23 +0200

    works check price on request

--- src/index.js
+++ src/index.js
@@ -2,6 +2,28 @@ const express = require('express');
 require('dotenv').config();
 const firebase = require('firebase');
 
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+
+const scrape = html => {
+  const $ = cheerio.load(html);
+  const result = $('div.first > .pr > p > strong > a').text();
+  const price =
+    result
+      .split(' ')
+      .filter(p => p !== 'Kč')
+      .join('') * 1;
+
+  return price;
+};
+
+const checkOne = async item => {
+  const html = await rp(item.url);
+  const price = await scrape(html);
+  const updated = { ...item, price };
+  return updated;
+};
+
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
   authDomain: 'wait-for-cheaper.firebaseapp.com',
@@ -27,7 +49,11 @@ const app = express();
 const port = process.env.PORT || 3131;
 
 app.get('/', (req, res) => {
-  res.json(list);
+  const prices = list.map(thing => checkOne(thing));
+
+  Promise.all(prices)
+    .then(done => res.json(done))
+    .catch(error => console.error(error));
 });
 
 app.listen(port, () =>
