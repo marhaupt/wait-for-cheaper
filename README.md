d1e36f36d8fc4b4c396273665f4e4b917e84a872
Martin Haupt <martin23haupt@gmail.com>
2019-06-04 21:16:09 +0200

    loading list from firebase

---
+++ README.md
@@ -0,0 +1,15 @@
+# Basic setup
+
+```
+npm init
+npm i express
+npm i D eslint prettier...
+eslint --init
+```
+
+# Firebase
+
+1. Create new project
+2. Database > Realtime DB and leave public for now
+3. `npm i firebase`
+4. some setup and snapshot and... you know what? [Look here](https://firebase.google.com/docs/database/web/lists-of-data)
--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
-var express = require('express');
+const express = require('express');
+require('dotenv').config();
+const firebase = require('firebase');
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: 'wait-for-cheaper.firebaseapp.com',
+  databaseURL: 'https://wait-for-cheaper.firebaseio.com',
+  projectId: 'wait-for-cheaper',
+  storageBucket: 'wait-for-cheaper.appspot.com',
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+};
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+const stuff = database.ref('stuff');
+
+let list = [];
+
+stuff.once('value', snapshot => {
+  list = snapshot.val();
+});
 
 const app = express();
 
 const port = process.env.PORT || 3131;
 
 app.get('/', (req, res) => {
-  res.json({ resp: 'onse' });
+  res.json(list);
 });
 
 app.listen(port, () =>
